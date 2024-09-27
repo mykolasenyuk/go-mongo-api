160bf50149fab467bf1f7ff05534968a29c26482
Mykola Senyuk <73036879+mykolasenyuk@users.noreply.github.com>
2024-09-27 03:24:59 +0300

    init

---
+++ handlers/router.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+type Response struct {
+	Msg  string
+	Code int
+}
+
+func CreateRouter() chi.Mux {
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTION"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CRSF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+}
